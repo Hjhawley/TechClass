1004e73b0e11bc9378ff1ea7a9a42553c03d35c0
Hayden Hawley <hjhawley7@gmail.com>
2022-08-23 13:16:07 -0600

    Introduction

---
+++ Intro/NewToPython.py
@@ -0,0 +1,10 @@
+# This is a comment.
+
+"This is a string."
+
+print("This is a print statement.")
+
+def dumbFunction(word, number): # <--- This is a function.
+    print(word * number)
+
+dumbFunction("Cheese", 3) # <--- This is a function call.
