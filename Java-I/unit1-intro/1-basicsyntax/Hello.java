352941454ec6c0d53972e1bf27ed4a8bf457210d
Hayden Hawley <hjhawley7@gmail.com>
2023-03-07 09:12:10 -0700

    Java I update

--- Java-I/unit1-intro/1-basicsyntax/Hello.java
+++ Java-I/unit1-intro/1-basicsyntax/Hello.java
@@ -1,3 +1,5 @@
-public static void main(String[] args) {
-    System.out.println("Hello, world!");
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
 }
