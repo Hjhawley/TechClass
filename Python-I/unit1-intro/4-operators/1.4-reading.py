9ff33fe86674b45b64e4070f237417f8fcf36ab1
Hayden Hawley <hjhawley7@gmail.com>
2023-03-23 17:39:00 -0600

    dummy

---
+++ Python-I/unit1-intro/4-operators/1.4-reading.py
@@ -0,0 +1,43 @@
+# A variable is a user-defined container associated with a value. When you declare
+# a variable, you give it a name (or label) and assign a value to it.
+# Variables can be changed or manipulated throughout the program.
+# Variables can be of any data type.
+# To declare a variable, you assign a value to a name using the = symbol.
+
+x = 42
+y = "Hello, world!"
+# In the above example, we declared two variables: x and y. We assigned
+# the integer value 42 to x and the string value "Hello, world!" to y.
+# We can then use these variables in our program by referencing their names.
+
+print(x) # This will output the value of x, which is 42.
+print(y) # This will output the value of y, which is "Hello, world!"
+# We can also reassign the values of variables by simply assigning a new
+# value to their names.
+
+# Python code executes from top-to-bottom. It carries out instructions line-by-line.
+# So we can declare a variable, do something with it, and then reassign it later.
+
+x = 10
+y = "Roo"
+# Now, the value of x is 10 and the value of y is "Roo".
+
+print(x) # This will output the new value of x, which is 10.
+print(y) # This will output the new value of y, which is "Roo".
+
+a = 2
+b = 3
+c = a + b
+# In this example, we assigned the integer value 2 to a, the integer value
+# 3 to b, and the result of the expression a + b (which is 5) to c.
+print(c) # This will output the value of c, which is 5.
+
+# It's important to note that variable names are case-sensitive, meaning that
+# "a" and "A" are two different variables. Variable names must start with a letter
+# or underscore, and can only contain letters, underscores, and numbers.
+
+# It's good practice to use descriptive variable names that reflect the purpose of
+# the data they are storing. This makes your code easier to read and understand.
+# For example, instead of using a variable name like "x", you could use something
+# more descriptive like "num_apples" if the variable is storing the number of
+# apples in a basket.
