22bdd0e7da9127569bf06f833f883dfd467d48b6
Hayden Hawley <hjhawley7@gmail.com>
2023-06-29 11:06:19 -0600

    Regex fix

--- Python-I/unit3-advanced/3.6-regex.py
+++ Python-I/unit3-advanced/3.6-regex.py
@@ -1,6 +1,6 @@
 # Regular Expressions
 
-# A regular expression, also known as regex or regexp, is a sequence of characters
+# A regular expression, also known as regex, is a sequence of characters
 # that defines a search pattern. It can be used to check if a string contains
 # the specified search pattern.
 
@@ -11,22 +11,25 @@ import re
 # The 're' module offers a set of functions that allows us to search a string for
 # a match:
 
-# The 'findall' function returns a list containing all matches:
-print(re.findall("a", "Hello, my name is Ana."))  # prints ['a', 'a', 'a', 'a']
+# The 'findall' function returns a list containing all non-overlapping matches:
+print(re.findall("a", "Hello, my name is Ana."))  # prints ['a', 'a']
 
 # The 'search' function searches the string for a match, and returns a Match object
 # if there is a match. If there is more than one match, only the first occurrence 
 # of the match will be returned:
-print(re.search("a", "Hello, my name is Ana."))  # prints <re.Match object; span=(12, 13), match='a'>
+match = re.search("a", "Hello, my name is Ana.")
+print(match)  # prints <re.Match object; span=(11, 12), match='a'>
 
-# The 'sub' function replaces the matches with the text of your choice:
-print(re.sub("a", "X", "Hello, my name is Ana."))  # prints 'Hello, my nXme is AnX.'
+# The 'sub' function replaces all the occurrences of the match with the text of your choice:
+print(re.sub("a", "X", "Hello, my name is Ana."))  # prints 'Hello, my nXme is XnX.'
 
 # We can use metacharacters, like [a-z] or [0-9], to find specific types of patterns:
-print(re.findall("[a-z]", "Hello, my name is Ana."))  # prints all lowercase letters
-print(re.findall("[0-9]", "I have 2 apples and 3 oranges."))  # prints all digits
+# The following line prints all lowercase letters in the string
+print(re.findall("[a-z]", "Hello, my name is Ana."))
+# The following line prints all digits in the string
+print(re.findall("[0-9]", "I have 2 apples and 3 oranges."))
 
-# The 'split' function returns a list where the string has been split at each match:
+# The 'split' function returns a list where the string has been split at each match of the pattern:
 print(re.split("\s", "Split on spaces."))  # prints ['Split', 'on', 'spaces.']
 
 # Regular expressions can get much more complex, but this is a basic introduction.
