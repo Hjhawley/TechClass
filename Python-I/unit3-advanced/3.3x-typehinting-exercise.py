0f6a5391a4447b73d6e34892893d55200668a8e6
Hayden Hawley <hjhawley7@gmail.com>
2023-06-30 11:26:18 -0600

    restructure

---
+++ Python-I/unit3-advanced/3.3x-typehinting-exercise.py
@@ -0,0 +1,10 @@
+# Add type hints to the following functions.
+
+def concatenate_strings(s1, s2):
+    return s1 + s2
+
+# After adding the type hints, run the code to check that 
+# it works as expected.
+
+s = concatenate_strings("Hello ", "world!")
+print(s)
---
+++ Python-I/unit3-advanced/3.6x-regex-exercise.py
@@ -0,0 +1,13 @@
+# Import and use the 're' module to perform some tasks on this string:
+
+text = "The quick brown fox jumps over the lazy dog 123."
+
+# 1. Use 'findall' to find all occurrences of the letter 'o' in the string.
+
+# 2. Use 'search' to find the first occurrence of a digit number.
+
+# 3. Use 'sub' to replace all whitespace characters with an underscore (_).
+
+# 4. Use 'findall' with a metacharacter to find all lowercase letters.
+
+# 5. Use 'split' to split the string at each whitespace character.
