5ca6aabaa48c42aa68ecdb44e3856cf084040dee
Hayden <hjhawley7@gmail.com>
2024-01-12 11:00:53 -0700

    1.1 classes

--- Python-II/unit1-classes/1.1-classes.py
+++ Python-II/unit1-classes/1.1-classes.py
@@ -7,18 +7,30 @@
 class Car:
     # The __init__ method is called the constructor. It's used for initializing
     # the attributes of the class.
-    def __init__(self, brand, model, year):
+    def __init__(self, brand, model, year, color):
         self.brand = brand  # Data member
         self.model = model  # Data member
         self.year = year    # Data member
+        self.color = color  # Data member
 
     # A method for displaying car information
     def display_info(self):
-        return f"Car: {self.brand} {self.model}, Year: {self.year}"
+        return f"Car: {self.color} {self.year} {self.brand} {self.model}"
+
+    # A method to update the year
+    def update_year(self, new_year):
+        self.year = new_year
 
 # Creating an Object
 # To create an object, you call the class name followed by the data members you want to
 # pass to the __init__ method as parameters.
 
-my_car = Car("Toyota", "Corolla", 2020)  # Creating a Car object
-print(my_car.display_info())  # Output: Car: Toyota Corolla, Year: 2020
+your_car = Car("Toyota", "Corolla", 2019, "Silver")  # Creating a Car object
+print(your_car.display_info())  # Silver 2019 Toyota Corolla
+
+# Objects can have their attributes updated using methods.
+
+my_car = Car("Honda", "Accord", 2003, "White")
+print(my_car.display_info())    # Car: White 2003 Honda Accord
+my_car.update_year(2021)        # Year updated to 2021
+print(my_car.display_info())    # Car: White 2021 Honda Accord
