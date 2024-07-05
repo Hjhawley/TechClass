e680caf6e5215a10a5a42b00a7bf7a03e0ffed79
Hayden <hjhawley7@gmail.com>
2024-07-05 10:32:11 -0600

    Classes and objects

--- Python-II/unit1-classes/1.1-classes.py
+++ Python-II/unit1-classes/1.1-classes.py
@@ -5,20 +5,20 @@
 
 # Defining a Simple Class
 class Car:
-    # The __init__ method is called the constructor. It's used for initializing
-    # the attributes of the class.
     def __init__(self, brand, model, year, color):
+        # The 'self' parameter is a reference to the current instance of the class.
+        # It's used to access variables that belong to the class.
         self.brand = brand  # Data member
         self.model = model  # Data member
         self.year = year    # Data member
         self.color = color  # Data member
 
-    # A method for displaying car information
     def display_info(self):
+        # Returns the car's information as a formatted string.
         return f"Car: {self.color} {self.year} {self.brand} {self.model}"
 
-    # A method to update the year
     def update_year(self, new_year):
+        # Updates the year of the car.
         self.year = new_year
 
 # Creating an Object
@@ -26,11 +26,18 @@ class Car:
 # pass to the __init__ method as parameters.
 
 your_car = Car("Chevrolet", "Impala", 2015, "Silver")  # Creating a Car object
-print(your_car.display_info())  # Silver 2015 Chevrolet Impala
+print(your_car.display_info())  # Output should be 'Car: Silver 2015 Chevrolet Impala'
 
 # Objects can have their attributes updated using methods.
-
 my_car = Car("Honda", "Accord", 2003, "White")
-print(my_car.display_info())    # Car: White 2003 Honda Accord
-my_car.update_year(2021)        # Year updated to 2021
-print(my_car.display_info())    # Car: White 2021 Honda Accord
+print(my_car.display_info())  # Output should be 'Car: White 2003 Honda Accord'
+my_car.update_year(2021)      # Year updated to 2021
+print(my_car.display_info())  # Output should be 'Car: White 2021 Honda Accord'
+
+# Accessing attributes directly
+print(my_car.brand)  # Output should be 'Honda'
+print(my_car.model)  # Output should be 'Accord'
+
+# You can also modify attributes directly, but it's better practice to use methods.
+my_car.color = "Black"
+print(my_car.display_info())  # Output: Car: Black 2021 Honda Accord
