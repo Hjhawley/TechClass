858a3a1e8a83c7936f8c476ad344cf2d8bcb5132
Hayden <hjhawley7@gmail.com>
2024-07-12 12:59:50 -0600

    Duck typing

--- Python-II/unit1-classes/1.4-polymorphism.py
+++ Python-II/unit1-classes/1.4-polymorphism.py
@@ -5,17 +5,21 @@
 # This is useful in cases where different types of objects share a common interface but
 # implement their behavior differently.
 
+# This is also called "Duck typing".
+# "If it walks like a duck and it quacks like a duck, it must be a duck."
+# If a derived class is able to use a method that's called on it, it can use it.
+
 # For example:
-# Parent class
+# Base class
 class Animal:
     def __init__(self, name):
         self.name = name
 
     def speak(self):
         # This method is intended to be overridden in derived classes.
-        raise NotImplementedError("Child class must implement this method.")
+        raise NotImplementedError("Subclasses must implement this method")
 
-# Child classes
+# Subclass
 class Dog(Animal):
     def speak(self):
         return f"{self.name} says 'Woof'"
@@ -45,4 +49,4 @@ for animal in animals:
 # Petey says 'Chirp'
 
 # Notice how polymorphism allows us to call the 'speak' method on each object,
-# and each object responds appropriately according to its own implementation of the method.
+# and each object responds according to its own implementation of the method.
