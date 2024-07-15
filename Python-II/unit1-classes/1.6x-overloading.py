411d6efd0c021e95b34c44bfe20b8c5b0c91189c
Hayden <hjhawley7@gmail.com>
2024-07-15 11:25:06 -0600

    Overloading exercise

--- Python-II/unit1-classes/1.6x-overloading.py
+++ Python-II/unit1-classes/1.6x-overloading.py
@@ -8,21 +8,23 @@ class BankAccount:
 
     def __add__(self, other):
         return BankAccount(self.balance + other.balance)
+
     
-    # Define a method here that overloads the - operator
+    # Define a method here that overloads the - operator using __sub__
+    # It should take a BankAccount instance (self) and a numeric value (amount)
     
+
     def __str__(self):
         return f"BankAccount(balance: ${self.balance:.2f})"
 
 # After you define the __sub__ method, the following code should work.
 
-# Create bank accounts
-account1 = BankAccount(1000.50)
-account2 = BankAccount(2500.75)
+# Create a bank account
+account = BankAccount(1000.45)
 
-# Transfer money between accounts using the overloaded - operator
-account1_after_withdrawal = account1 - 300.00
-print(account1_after_withdrawal)  # Output: BankAccount(balance: $700.50)
+# Transfer money from the account using the overloaded - operator
+account_after_withdrawal = account - 300.00
+print(account_after_withdrawal)  # Output: BankAccount(balance: $700.45)
 
 # If you see 'TypeError: unsupported operand type(s) for -: 'BankAccount' and 'float''
 # that means you have a mistake in your implementation. Keep at it!
