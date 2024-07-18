a8b850d71eaea4d3b926f96a757acae20014c5f4
Hayden <hjhawley7@gmail.com>
2024-07-18 10:38:45 -0600

    Improved intro

--- Python-I/unit1-intro/1.1-syntax.py
+++ Python-I/unit1-intro/1.1-syntax.py
@@ -1,44 +1,51 @@
 # Basic Python Syntax
 
-# In Python, each line of code contains its own separate instructions.
-# These instructions carry out from top to bottom.
+# In Python, each line of code contains its own instruction.
+# These instructions are executed from top to bottom.
 
-# In languages like Java and C++, indentation does not affect the
-# program and is purely for the sake of user readability. However,
-# indentation does matter in Python and later on we'll see some examples.
+# In some programming languages like Java and C++, indentation (spaces or tabs
+# at the beginning of a line) is mainly for readability and doesn't affect the 
+# program. However, spacing and indentation does matter in Python.
 
-# <--- Hash symbols are used to write comments.
-# Comments are ignored by the Python interpreter. They are meant for humans.
-# Usually they clarify the purpose of a particular block of code.
-# They can also be used to temporarily disable lines of code for testing purposes.
+# <--- Hash symbols are used to write comments, like this.
+# Comments are ignored by Python when the code runs. They are meant for humans.
+# Comments can help explain what the code does, or temporarily disable parts of
+# code for testing purposes.
 
 '''
-Triple quotes like this can also be used to write multi-line comments
-like this.
+Triple quotes can also be used to write multi-line comments
+like this one.
 '''
 
-# Throughout this course I'll be using comments to explain concepts.
+# Throughout this course, I'll use comments to explain programming concepts.
 
 print("Hello, world!")
 # The above line of code should look just like the very first program you wrote.
-# The print() function is a built-in Python function that allows you to
-# output text to the console (aka the terminal).
+# When run, it prints the text "Hello, world!" to the terminal down below.
+# The terminal is also sometimes called the 'console'.
 
-# Inside the parentheses is the value we want to print; in this case, a
-# string of characters that spell "Hello, world!"
+# Inside the parentheses is the value we want to print; in this case, a string
+# of characters that spell "Hello, world!"
+# A string is a sequence of characters enclosed in quotes.
 
-# This value is called the argument. An argument is any value that is
-# passed to a function when it is called. Some functions require multiple
-# arguments, and some don't require any at all.
+# The value inside the parentheses is called an argument.
+# An argument is any value passed to a function when it is called.
+# Some functions require multiple arguments, and some don't require any at all.
 
 print(2 + 2)
-# This time, instead of a string of characters we are passing a math
-# expression as the argument to the print() function. The expression
-# "2 + 2" will be calculated by Python and the result, 4, will be
-# printed to the console.
+# This time, instead of a string, we are passing a math expression to the
+# print() function. The expression "2 + 2" will be calculated by Python and the
+# result, 4, will be printed to the terminal.
 
-# As you can see, the print() function can accept many types of arguments,
-# not just strings.
+# As you can see, the print() function can accept many types of input, not just
+# strings. Many other programming languages require you to convert the input to
+# a string before printing it, but Python handles this automatically.
 
-# print() is just one of many built-in Python functions. Programmers can
-# also define their own functions. We'll get into this later.
+# Here are a few more examples of using the print() function:
+print("Python is fun!")
+print(10 * 3)
+print("The sum of 5 and 7 is", 5 + 7)
+
+# The print() function is just one of many built-in Python functions.
+# Programmers can also define their own functions. We'll learn how to do that
+# very soon, and that's where programming gets really fun!
